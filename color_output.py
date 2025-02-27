f9da1b1dc126bbee67aca1116c5ce540c2a11902
landix <landixbtw@gmail.com>
2025-02-27 16:33:16 +0100

    feat: added a check if the user has the 'config.json' file, if the session_timer field does not exist, or something is wrong, the value defaults to white.
    Signed-off-by: landix <landixbtw@gmail.com>

--- color_output.py
+++ color_output.py
@@ -12,13 +12,17 @@ import json
 # TODO: Not sure how to implement multiple colors to return from the function maybe an array ?
 
 def hex_to_rbg():
- with open("config.json", "r") as file:
-        config = json.load(file)
-        theme = config["general"]["theme"]
+    hex_code = "000000"
+    try:
+        with open("config.json", "r") as file:
+            config = json.load(file)
+            session_timer = config.get("general", {}).get("session_timer", "#ffffff") # the last value is the default value if there is none found
+            hex_code = session_timer.lstrip("#")
+    except (FileNotFoundError, json.JSONDecodeError):
+        hex_code = ("ffffff")
+    return tuple(int(hex_code[i:i+2], 16) for i in (0,2,4))
 
-        hex_code = theme.lstrip("#")
-        return tuple(int(hex_code[i:i+2], 16) for i in (0,2,4))
-    
 def colored_output(input):
     r, g, b = hex_to_rbg()
     return f"\033[38;2;{r};{g};{b}m{input}\033[0m"
+
